3de6eb015e9c472353c0c8d18ff06d56ff3b82a5
kanade-o <sotaohnuma@gmail.com>
2025-06-08 20:44:28 +0900

    Jun 8

--- abc373/b/main.py
+++ abc373/b/main.py
@@ -1,6 +1,9 @@
 matrix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 S = input()
 
-for i in range(26):
+count = 0
+for i in range(25):
   before = S.find(matrix[i])
   after = S.find(matrix[i+1])
+  count += abs(after - before)
+print(count)
